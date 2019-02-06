47ce3cedb76dd7078a9c6f1649244f7cbfd97e92
Denys Hriaznov <hriaznov.dev@gmail.com>
2019-02-06 21:11:48 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+// Create a server with a host and port
+const server = Hapi.server({
+  host: 'localhost',
+  port: 8000
+});
+
+// Add the route
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request, h) {
+    return 'hello';
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/hello',
+  handler: function(request, h) {
+    return 'hello world';
+  }
+});
+
+// Start the server
+const start = async () => {
+  try {
+    await server.start();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log('Server running at:', server.info.uri);
+};
+
+start();
